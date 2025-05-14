200475e84f1be50ccf43fadcb4cc89c0d2d880a9
TimoAepli <timo.aepli@edu.gbssg.ch>
2025-05-14 11:35:56 +0200

    Aufgabe 3.2

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,4 +3,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/check", () => {
+/* Code zur Prüfung der DB ...*/
+return "Zugriff auf MongDB ok.";
+});
+
 app.Run();
